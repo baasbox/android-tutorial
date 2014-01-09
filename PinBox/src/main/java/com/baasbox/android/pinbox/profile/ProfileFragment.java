72a88ddd226be2ef14da65c83b58e3156232c294
aKtor <eliantor@gmail.com>
2014-01-09 17:31:12 +0100

    setup baasdeps

---
+++ PinBox/src/main/java/com/baasbox/android/pinbox/profile/ProfileFragment.java
@@ -0,0 +1,9 @@
+package com.baasbox.android.pinbox.profile;
+
+import com.baasbox.android.pinbox.common.BaseFragment;
+
+/**
+ * Created by eto on 26/12/13.
+ */
+public class ProfileFragment extends BaseFragment {
+}
